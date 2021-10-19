35b29f32ec8ff1bc23037d8a9a3f6018aaaa2426
amineZargouni <mohamedamine.zargouni@esprit.tn>
2021-10-20 00:50:10 +0100

    ContratDTO

--- src/main/java/tn/esprit/spring/services/ContratServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/ContratServiceImpl.java
@@ -2,10 +2,13 @@ package tn.esprit.spring.services;
 
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import tn.esprit.spring.entities.Contrat;
+import tn.esprit.spring.entities.ContratDTO;
+
 import tn.esprit.spring.repository.ContratRepository;
 
 @Service
@@ -14,10 +17,25 @@ public class ContratServiceImpl implements IContratService {
 
 	@Autowired
 	ContratRepository contratRepository;
+	private ModelMapper mapper;
 
 
 	public List<Contrat> getAllContrats() {
 		return (List<Contrat>) contratRepository.findAll();
+	}
+
+
+	@Override
+	public Contrat mapToEntity(ContratDTO contratDTO) {
+		Contrat contrat = mapper.map(contratDTO, Contrat.class);
+		return contrat;
+	}
+
+
+	@Override
+	public ContratDTO mapToDTO(Contrat contrat) {
+		ContratDTO contratDTO = mapper.map(contrat, ContratDTO.class);
+		return contratDTO;
 	}
 
 }
